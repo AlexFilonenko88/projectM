dd06e2ffbc5cb04f6e15ac9beb31f2baca6f4fa9
AlexFilonenko88 <87144003+AlexFilonenko88@users.noreply.github.com>
2023-09-11 08:37:46 +0300

    add file function

---
+++ functions.php
@@ -0,0 +1,18 @@
+<?
+function get_user_by_email($email){
+    $pdo = new PDO ("mysql:host=localhost;dbname=projectm", "root", "");
+    $sql = "SELECT * FROM projectm WHERE email=:email";
+    $statement = $pdo->prepare($sql);
+    $statement->execute(['email' => $email]);
+    return $res = $statement->fetch(PDO:: FETCH_ASSOC);
+};
+
+function add_user($email, $password){
+
+    $password_hashed = password_hash($password, PASSWORD_DEFAULT);
+    
+    $pdo = new PDO ("mysql:host=localhost;dbname=projectm", "root", "");
+    $sql = "INSERT INTO projectm (email, password) VALUES (:email, :password)";
+    $statement = $pdo->prepare($sql);
+    return $statement->execute(['email' => $email, 'password' => $password_hashed]);
+    };
--- handler_page_register.php
+++ handler_page_register.php
@@ -1,28 +1,22 @@
 <?php
 session_start();
 
+require "functions.php";
+
 $email = $_POST['email'];
 $password = $_POST["password"];
 
-$pdo = new PDO ("mysql:host=localhost;dbname=projectm", "root", "");
-$sql = "SELECT * FROM projectm WHERE email=:email";
-$statement = $pdo->prepare($sql);
-$statement->execute(['email' => $email]);
-$res = $statement->fetch(PDO:: FETCH_ASSOC);
+$user = get_user_by_email($email);
 
-if(!empty($res)){
+if(!empty($user)){
     $_SESSION['error'] = "Этот эл. адрес уже занят другим пользователем.";
 
     header("Location: /page_register.php");
     die();
 };
 
-$password_hashed = password_hash($password, PASSWORD_DEFAULT);
+add_user($email, $password);
 
-$sql = "INSERT INTO projectm (email, password) VALUES (:email, :password)";
-$statement = $pdo->prepare($sql);
-$statement->execute(['email' => $email, 'password' => $password_hashed]);
-
-$_SESSION['success'] = "Регистриция успешна!";
+$_SESSION['success'] = 'Пользователь зарегистрирован!';
 
 header("Location: /page_login.php");
