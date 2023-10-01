82d99803909217b0d9bc74dae83d3cfa402fa471
AlexFilonenko88 <af88_88@mail.ru>
2023-10-01 18:26:54 +0300

    modified file function.php

--- functions.php
+++ functions.php
@@ -95,3 +95,23 @@ function is_equel ($user, $current_user) { // хозяин или нет
 };
 
 // файл create_users.php
+
+function add_user_one ($email, $password) {
+
+}
+
+function edit_information ($username, $job_title, $phone, $address) {
+
+}
+
+function set_status ($status) {
+
+}
+
+function upload_avatar ($image) {
+
+}
+
+function add_social_links ($telegram, $instagram, $vk) {
+    
+}
